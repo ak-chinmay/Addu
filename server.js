e65ccc5a8d07b9c067dd1ff5c0213eba53264086
chinmay <chinmay.darth@gmail.com>
2015-04-08 23:59:22 +0530

    added docReady feature

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/lib'));
 
 
+//List of users
+users=[
+    {name:'ABC',emailid:'abc@gmail.com',message:'Ads from ABC'},
+    {name:'PQR',emailid:'pqr@yahoo.com',message:'Ads from PQR'}];
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -13,9 +19,20 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log("connected to "+socket.id);
-  socket.on('chat message', function(msg){
-    io.to(socket.id).emit('chat message', msg);
+  socket.on('Req', function(data){
+      //*Biz logic
+      users.forEach(function(u){
+
+          if(u.emailid==data.emailid)
+          {
+ 
+    io.to(socket.id).emit('Res',u);
+//           break;   
+          }
+          
   });
+});
+    
 });
 
 http.listen(3000, function(){
