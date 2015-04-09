40a99a84464e83cf808cbeaa5ffc12ffb0dd2ac3
unknown <carankal@DIN16001981.corp.capgemini.com>
2015-04-09 17:46:56 +0530

    Added client side calling feature

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Server module of Ad server ClairV
+
 var express=require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -5,20 +7,42 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/lib'));
 
 
+//Send Error message
+function getErrorMessage(msg)
+{
+if(msg!=undefined)
+{
+    return data={
+message:msg  };
+}else{
+return undefined;
+}
+    
+  
+}
+
+
+
 //List of users
 users=[
-    {name:'ABC',emailid:'abc@gmail.com',message:'Ads from ABC'},
-    {name:'PQR',emailid:'pqr@yahoo.com',message:'Ads from PQR'}];
+    {name:'Vodafone',emailid:'vodafone@vodafone.com',message:'Ads from Vodafone'},
+    {name:'Idea',emailid:'idea@idea.com',message:'Ads from Idea'},
+    {name:'Airtel',emailid:'airtel@airtel.com',message:'Ads from Airtel'}];
 
 
-
+//Displating page
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 
+//Initial Connection
 io.on('connection', function(socket){
+    
+    flag=false;
+    
     console.log("connected to "+socket.id);
+    
   socket.on('Req', function(data){
       //*Biz logic
       users.forEach(function(u){
@@ -27,11 +51,22 @@ io.on('connection', function(socket){
           {
  
     io.to(socket.id).emit('Res',u);
-//           break;   
+              flag=true;
           }
           
   });
+      
+      //Authentication Mechanism
+      if(!flag)
+      {
+          
+          io.to(socket.id).emit('ErrorRes',getErrorMessage('Authentication failed'));
+      }
+          
+          
 });
+    
+  
     
 });
 
